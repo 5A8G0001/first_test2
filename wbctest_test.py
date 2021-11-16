21827aeb5f874cea19085e8f8c96aade18f5539b
5A8G0001 <91514918+5A8G0001@users.noreply.github.com>
2021-11-16 22:48:49 +0800

    登入帳號小幫手完成 地下城功能完成 神殿功能、聖跡功能、探索經驗值瑪那功能 基本功能全部完成，可對部分未用try地方做優化、可對速度穩定性作優化 (new)網路測試基本功能完成

--- wbctest_test.py
+++ wbctest_test.py
@@ -1,30 +1,104 @@
-#from selenium import  webdriver
-import selenium.webdriver.common.devtools.v85.css
-
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from  selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+import time
+from airtest.core.api import *
+
+
+# 基本上傳完成
 driver = Chrome(executable_path = 'D:\Chrome\chromedriver_win32\chromedriver.exe') #提供chromedriver路徑
 
-to_FlipClass = 'https://flipclass.stust.edu.tw/'
+to_FlipClass = 'https://flipclass.stust.edu.tw/'  # flipclass網址
 to_google = 'https://www.google.com/'
-driver.get(to_FlipClass)  # 開啟網址
-'''search = driver.find_element_by_name("q")
-search.send_keys('靈夢')
-search.send_keys(Keys.ENTER)'''
-login_Account = driver.find_element_by_name("account")  # 找到標籤
-login_Account.send_keys("5A8G0001")
-login_Password = driver.find_element_by_name("password")  # 找到標籤
-login_Password.send_keys("a0973720803")
-login_Password.send_keys(Keys.ENTER)
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//html/body/div[2]/div/div[3]/div[2]/div/div[2]/div/div[3]/div/div/div[2]/div[1]/div[1]/div[1]/div/ul/li[2]/div[3]/div/div[2]/a/span[2]"))).click()
+
+FlipAccount = ''
+FlipPassword = ''
+driver.get(to_FlipClass)  # 使用瀏覽器開啟網址
+
+login_Account = driver.find_element_by_name("account")  # 找到帳號標籤
+login_Account.send_keys("5A8G0001")  # 輸入帳號
+login_Password = driver.find_element_by_name("password")  # 找到密碼標籤
+login_Password.send_keys("a0973720803") # 輸入密碼
+login_Password.send_keys(Keys.ENTER)  # 在密碼輸入盒按Enter
 
 
 # /course/homework/課程編號(29396) span[text()='作業名稱']
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='text']//a[contains(@href, '/course/homework/28386')]/span[text()='作業6：小畫家']"))).click()
-#cls.send_keys(Keys.ENTER)
-#class_name = driver.find_element_by_name("text")
-#print(driver)
-#driver.close()//*[@id="xbox-inline"]/div[7]/div/a/span
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='text']//span[text()='AddProj']"))).click() # 可以省略到只有span  # 點取作業名稱的作業
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='text-center fs-margin-default']//span[text()='交作業']"))).click()  # 進入交作業畫面
+
+
+
+driver.switch_to.frame(driver.find_element_by_class_name('fs-modal-iframe')) # 交作業畫面為一個內嵌的iframe 要 switch到裡面
+
+
+path1 = r'D:\led12.py' # 要上傳的檔案的絕對路徑
+
+#tt1 = driver.find_element_by_css_selector('.close').click()
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="上傳檔案"]'))).click()  # 點擊上傳檔案按鈕
+
+time.sleep(2)
+
+text1 = driver.find_element_by_name('files[]').send_keys(path1)  # 因為學校上傳檔案的方式是input，故可以直接send_keys(路徑)
+time.sleep(5) # 不可在上傳過程中做其他事
+WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.CLASS_NAME, 'close'))).click()  # 關閉上傳畫面
+WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="media-edit-form"]/div[7]/div/button[1]/span'))).click()  # 繳交 ， 同時可能會離開iframe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#bt = driver.find_element_by_css_selector('button.btn btn-default')
+
+#te1 = driver.find_element_by_xpath('//*[@id="action"]')
+#print(te1)
+
+#WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='page-main']"))).click()
+#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='btn btn-default']//span[text()='上傳檔案']"))).click()
+#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='fs-list']//span[text()='上傳檔案']"))).click()
+
+#driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div/div/div[2]/div/form/div[5]/div/div/button').click()
+#driver.find_element_by_xpath('//*[@id="__button61924bceb5527_1"]').click()
+#driver.find_element_by_xpath('//*[@aria-controls="上傳檔案"]').click()
+
+#inputbox = driver.find_element_by_xpath('/html/body')  # 文字輸入處
+#inputbox.click()
+#time.sleep(2)
+#inputbox.send_keys(Keys.ENTER)
+#driver.find_element_by_xpath('//*[@value="葉啟源_HW8"]').click()
+
+#WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html"))).click()
+
+
+#upbt = driver.find_element_by_xpath("//*[@id=__button61923b0c9ec3a_1]/span")
+
+
+
