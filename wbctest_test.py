967105c6d0957c8be20105d5e4655d9fb7a15a3a
5A8G0001 <91514918+5A8G0001@users.noreply.github.com>
2021-11-20 18:37:00 +0800

    登入帳號小幫手完成 地下城功能完成 神殿功能、聖跡功能、探索經驗值瑪那功能 基本功能全部完成，可對部分未用try地方做優化、可對速度穩定性作優化 (new)交作業測試功能完成 2021/11/20 18:36

--- wbctest_test.py
+++ wbctest_test.py
@@ -3,49 +3,70 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from  selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
+import tkinter as tk
 import time
-from airtest.core.api import *
 
 
-# 基本上傳完成
-driver = Chrome(executable_path = 'D:\Chrome\chromedriver_win32\chromedriver.exe') #提供chromedriver路徑
+def test_web_start():
+    # 基本上傳完成
+    driver = Chrome(executable_path='D:\Chrome\chromedriver_win32\chromedriver.exe')  # 提供chromedriver路徑
 
-to_FlipClass = 'https://flipclass.stust.edu.tw/'  # flipclass網址
-to_google = 'https://www.google.com/'
+    to_FlipClass = 'https://flipclass.stust.edu.tw/'  # flipclass網址
+    to_google = 'https://www.google.com/'
 
-FlipAccount = ''
-FlipPassword = ''
-driver.get(to_FlipClass)  # 使用瀏覽器開啟網址
+    FlipAccount = ''
+    FlipPassword = ''
+    driver.get(to_FlipClass)  # 使用瀏覽器開啟網址
 
-login_Account = driver.find_element_by_name("account")  # 找到帳號標籤
-login_Account.send_keys("5A8G0001")  # 輸入帳號
-login_Password = driver.find_element_by_name("password")  # 找到密碼標籤
-login_Password.send_keys("a0973720803") # 輸入密碼
-login_Password.send_keys(Keys.ENTER)  # 在密碼輸入盒按Enter
+    login_Account = driver.find_element_by_name("account")  # 找到帳號標籤
+    login_Account.send_keys(account_Text.get())  # 輸入帳號
+    login_Password = driver.find_element_by_name("password")  # 找到密碼標籤
+    login_Password.send_keys(password_Text.get())  # 輸入密碼
+    login_Password.send_keys(Keys.ENTER)  # 在密碼輸入盒按Enter
+    work = "AddProj"  # 作業名稱
+    # /course/homework/課程編號(29396) span[text()='作業名稱']
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='text']//span[text()='%s']" % work))).click()  # 可以省略到只有span  # 點取作業名稱的作業
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='text-center fs-margin-default']//span[text()='交作業']"))).click()  # 進入交作業畫面
+
+    driver.switch_to.frame(driver.find_element_by_class_name('fs-modal-iframe'))  # 交作業畫面為一個內嵌的iframe 要 switch到裡面
+
+    path1 = r'D:\led12.py'  # 要上傳的檔案的絕對路徑
+
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="上傳檔案"]'))).click()  # 點擊上傳檔案按鈕
+
+    time.sleep(2)
+
+    driver.find_element_by_name('files[]').send_keys(path1)  # 因為學校上傳檔案的方式是input，故可以直接send_keys(路徑)
+    time.sleep(5)  # 不可在上傳過程中做其他事
+    WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.CLASS_NAME, 'close'))).click()  # 關閉上傳畫面
+    WebDriverWait(driver, 7).until(EC.element_to_be_clickable(
+        (By.XPATH, '//*[@id="media-edit-form"]/div[7]/div/button[1]/span'))).click()  # 繳交 ， 同時可能會離開iframe
 
 
-# /course/homework/課程編號(29396) span[text()='作業名稱']
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='text']//span[text()='AddProj']"))).click() # 可以省略到只有span  # 點取作業名稱的作業
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='text-center fs-margin-default']//span[text()='交作業']"))).click()  # 進入交作業畫面
+windows = tk.Tk()
+windows.geometry('500x500')
+
+account_Label = tk.Label(windows, text='帳號:')
+account_Label.grid(row=0, column=0)
+account_Text = tk.Entry(windows)
+account_Text.grid(row=0, column=1)
+
+account_Save = tk.Button(windows, text='儲存帳號密碼')
+
+password_Label = tk.Label(windows, text='密碼')
+password_Text = tk.Entry(windows)
+password_Label.grid(row=1, column=0)
+password_Text.grid(row=1, column=1)
+account_Save.grid(row=2,column=0)
+
+start_Button = tk.Button(windows, text='開始', command=lambda: test_web_start())
+start_Button.grid(row=2, column=1)
+
+windows.mainloop()
 
 
-
-driver.switch_to.frame(driver.find_element_by_class_name('fs-modal-iframe')) # 交作業畫面為一個內嵌的iframe 要 switch到裡面
-
-
-path1 = r'D:\led12.py' # 要上傳的檔案的絕對路徑
-
-#tt1 = driver.find_element_by_css_selector('.close').click()
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="上傳檔案"]'))).click()  # 點擊上傳檔案按鈕
-
-time.sleep(2)
-
-text1 = driver.find_element_by_name('files[]').send_keys(path1)  # 因為學校上傳檔案的方式是input，故可以直接send_keys(路徑)
-time.sleep(5) # 不可在上傳過程中做其他事
-WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.CLASS_NAME, 'close'))).click()  # 關閉上傳畫面
-WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="media-edit-form"]/div[7]/div/button[1]/span'))).click()  # 繳交 ， 同時可能會離開iframe
 
 
 
